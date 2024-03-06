1eeac561795bf0ec86f33aac9716f483bf0c77c0
arthurmyhra <arthur.myhra@ntnu.no>
2024-03-06 13:47:14 +0100

    Ryddet og la til flere tester

--- voxelarray2mesh.py
+++ voxelarray2mesh.py
@@ -12,7 +12,7 @@ class voxel2mesh:
     def copy(self):
         return self.mesh.copy()
 
-    def voxel_to_mesh(self, pitch=1.0, scaling=0.3):
+    def convert(self, pitch=1.0, scaling=0.003):
         # Convert the voxel array to a mesh
         mesh = trimesh.voxel.ops.matrix_to_marching_cubes(self.voxel_array, pitch=pitch)
         # Adjusting the mesh
@@ -23,6 +23,10 @@ class voxel2mesh:
         trimesh.repair.fix_inversion(mesh)
         trimesh.repair.fix_winding(mesh)
         self.mesh = mesh        
+        return self
+    
+    def scale(self, scaling=0.3):
+        self.mesh.apply_scale(scaling)
         return self
     
     def humphrey_smoothing(self, alpha=1, beta=1, iterations=100):
@@ -56,21 +60,25 @@ class voxel2mesh:
     def zbounds(self):
         bounds = self.mesh.bounds
         return [bounds[0][2],bounds[1][2]]
+    
+    def add_to_scene(self, scene):
+        scene.add_geometry(self.mesh)
+        return self
 
     def view(self):
         self.mesh.show()
 
-    def export_mesh(self, directory, file_name=0):
+    def export(self, directory, file_name=0, filetype="stl"):
         if not os.path.exists(directory):
             os.makedirs(directory)
         if file_name:
-            file_obj = directory + f"/{file_name}.stl"
+            file_obj = directory + f"/{file_name}.{filetype}"
         else:
-            file_obj = directory + f"/mesh{str(time.time())}.stl"
+            file_obj = directory + f"/mesh{str(time.time())}.{filetype}"
         trimesh.exchange.export.export_mesh(
             mesh=self.mesh,
             file_obj=file_obj,
-            file_type="stl"
+            file_type=filetype
         )
 
 
@@ -86,22 +94,45 @@ if __name__ == "__main__":
         os.makedirs(directory)
 
     # Generate mesh
-    mesh = voxel2mesh(voxel_array).voxel_to_mesh()
+    mesh = voxel2mesh(voxel_array).convert()
+
+    iterations = 500
+    lamb = 0.9
+    nu=lamb/(1+0.1*lamb)
+    t = time.time()
+    taubin = voxel2mesh(mesh=mesh.copy()).taubin_smoothing(lamb=lamb, nu=nu, iterations=iterations)
+    print(f"Taubin: {time.time()-t}s" )
+
+    """ t = time.time()
+    humphrey = voxel2mesh(mesh=mesh.copy()).humphrey_smoothing(alpha=0.01, beta=0.1, iterations=200).translate([mesh.xbounds()[1],0,0])
+    print(f"Humphrey: {time.time()-t}") """
+    """ t = time.time()
+    laplacian = voxel2mesh(mesh=mesh.copy()).laplacian_smoothing(lamb=0.8, iterations=100).translate([humphrey.xbounds()[1],0,0])
+    print(f"laplacian: {time.time()-t}") """
+    
+    """ekte = voxel2mesh(mesh=trimesh.exchange.load.load_mesh("Datasets/ShapeNet/model_normalized.obj")).translate([taubin.xbounds()[1]+0.55*mesh.xbounds()[1],0.5*taubin.ybounds()[1],0.4*taubin.zbounds()[1]]) """
+    #mesh.view()
 
     #Create scene
-    scene = trimesh.scene.scene.Scene()
-    scene.add_geometry(mesh.mesh)
+    """ scene = trimesh.scene.scene.Scene() """
 
     # Smooth mesh and add to scene
+    """ mesh.add_to_scene(scene)
+    humphrey.add_to_scene(scene)
+    laplacian.add_to_scene(scene)
+    taubin.add_to_scene(scene)
+    ekte.add_to_scene(scene) """
 
     # Humphrey smoothing
     """ xbounds = mesh.xbounds()
     ybounds = mesh.ybounds()
-    for beta in range(50, 1, -10):
-        for alpha in range(50, 1, -10):
+    for beta in range(100, 0, -20):
+        for alpha in range(100, 0, -20):
             temp_mesh = voxel2mesh(mesh=mesh.copy())
-            temp_mesh.humphrey_smoothing(alpha=alpha/100, beta=beta/100, iterations=50)
-            temp_mesh.translate([xbounds[1]+20,ybounds[1]+20,0])
+            t = time.time()
+            temp_mesh.humphrey_smoothing(alpha=alpha/1000, beta=beta/100, iterations=100)
+            print(f'alpha: {alpha/100}, beta: {beta/100}, time: {time.time()-t}')
+            temp_mesh.translate([xbounds[1],ybounds[1],0])
             xbounds = temp_mesh.xbounds()
             scene.add_geometry(temp_mesh.mesh)
         xbounds = mesh.xbounds()
@@ -109,23 +140,58 @@ if __name__ == "__main__":
     
     # Laplacian smoothing
     """ xbounds = mesh.xbounds()
-    for lamb in range(1, 10, 1):
-        temp_mesh = voxel2mesh(mesh=mesh.copy())
-        temp_mesh.laplacian_smoothing(lamb=lamb/10, iterations=50)
-        temp_mesh.translate([xbounds[1]+20,0,0])
-        xbounds = temp_mesh.xbounds()
-        scene.add_geometry(temp_mesh.mesh) """
+    for i in range(10, 100, 10):
+        for lamb in range(0, 100, 10):
+            temp_mesh = voxel2mesh(mesh=mesh.copy())
+            t = time.time()
+            temp_mesh.laplacian_smoothing(lamb=lamb/100, iterations=i)
+            print(f'lamb: {lamb/100}, iterations: {i}, time: {time.time()-t}')
+            temp_mesh.translate([xbounds[1]+20,0,0])
+            xbounds = temp_mesh.xbounds()
+            scene.add_geometry(temp_mesh.mesh) """
+    
+    
 
     # taubin smoothing
-    xbounds = mesh.xbounds()
+    """ xbounds = mesh.xbounds()
     for lamb in range(1, 10, 1):
         temp_mesh = voxel2mesh(mesh=mesh.copy())
         temp_mesh.taubin_smoothing(lamb=lamb/10, nu=lamb/10, iterations=50)
         temp_mesh.translate([xbounds[1]+20,0,0])
         xbounds = temp_mesh.xbounds()
-        scene.add_geometry(temp_mesh.mesh)
+        scene.add_geometry(temp_mesh.mesh) """
+    
+    """ xbounds = mesh.xbounds()
+    ybounds = mesh.ybounds()
+    for lamb in range(0, 100, 20):
+        for nu in [lamb/100, lamb/(100+0.1*lamb)]:
+            for i in range(100, 1000, 200):
+                temp_mesh = voxel2mesh(mesh=mesh.copy())
+                t = time.time()
+                temp_mesh.taubin_smoothing(lamb=lamb/100, nu=nu, iterations=i)
+                print(f'lamb: {lamb/100}, nu: {nu}, iterations: {i}, time: {time.time()-t}')
+                temp_mesh.translate([xbounds[1],ybounds[1],0])
+                xbounds = temp_mesh.xbounds()
+                scene.add_geometry(temp_mesh.mesh)
+            xbounds = mesh.xbounds()
+            ybounds = temp_mesh.ybounds() """
+    
+    """ xbounds = mesh.xbounds()
+    for i in [0, 1]:
+        iterations = [500, 2000]
+        lamb = [0.9, 0.45]
+        nu=lamb[i]/(1+0.1*lamb[i])
+        temp_mesh = voxel2mesh(mesh=mesh.copy())
+        t = time.time()
+        temp_mesh.taubin_smoothing(lamb=lamb[i], nu=nu, iterations=iterations[i])
+        print(f'lamb: {lamb[i]}, nu: {nu}, iterations: {iterations[i]}, time: {time.time()-t}')
+        temp_mesh.translate([xbounds[1],0,0])
+        xbounds = temp_mesh.xbounds()
+        scene.add_geometry(temp_mesh.mesh) """
     
     # View scene
-    scene.show()
+    """ scene.show() """
+    taubin.view()
 
-
+    # Export mesh
+    taubin.export(directory, file_name=f"taubin({lamb}-{nu}-{iterations})",filetype="obj")
